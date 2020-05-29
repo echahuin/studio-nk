d1bb7d02dfe386fa3468d026dbd2d26bdad4aed1
Edgarr <edgachahuinauccafuro@utn.frba.utn.edu.ar>
2020-05-29 11:19:09 -0300

    primera actualizacion studio nk

---
+++ src/components/CardsHome.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+    <b-row no-gutters>
+      <b-col md="6">
+        <b-card-img src="https://picsum.photos/400/400/?image=20" alt="Image" class="rounded-0"></b-card-img>
+      </b-col>
+      <b-col md="6">
+        <b-card-body :title="msg">
+          <b-card-text>
+           {{cont}}, {{msg}}
+          </b-card-text>
+        </b-card-body>
+      </b-col>
+    </b-row>
+  </b-card>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: String,
+    cont: []
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import './style.scss'
+
+// import style
+import 'swiper/css/swiper.css'
+
+Vue.use(VueAwesomeSwiper, /* { default options with global component } */)
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
